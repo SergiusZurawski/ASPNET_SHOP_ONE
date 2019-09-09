9793a60126d2aa8cc857323c6f0396987787bcab
Sergii Zhuravskyi <SZhuravskyi@navinet.net>
2019-09-09 18:00:07 +0100

    fix all

---
+++ WebAppOneAster/Controllers/SergController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppOneAster.Models;
+
+namespace WebAppOneAster.Controllers
+{
+    public class SergController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        
+        public ViewResult ListResponses(){
+            return View(Repository.Responses.Where(a => a.WillAttend == true));
+        }
+
+
+    }
+}
